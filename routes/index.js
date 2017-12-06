93502ae88828f9eb068be3434ca039200f26dad7
dzy <964660612@qq.com>
2017-12-06 22:31:15 +0800

    初始化

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,26 @@ var crypto = require('crypto');
 var token = 'myapp';
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  var signature = req.query.signature;
-  var timestamp = req.query.timestamp;
-  var nonce = req.query.nonce;
-  var echostr = req.query.echostr;
+  res.render('index', { title: 'Express' });
+  // var signature = req.query.signature;
+  // var timestamp = req.query.timestamp;
+  // var nonce = req.query.nonce;
+  // var echostr = req.query.echostr;
 
-  var array = new Array(token, timestamp, nonce);
-  array.sort();
-  var str = array.toString().replace(/,/g, '');
+  // var array = new Array(token, timestamp, nonce);
+  // array.sort();
+  // var str = array.toString().replace(/,/g, '');
 
-  var sha1Code = crypto.createHash('sha1');
-  var code = sha1Code.update(str, 'utf-8').digest('hex');
+  // var sha1Code = crypto.createHash('sha1');
+  // var code = sha1Code.update(str, 'utf-8').digest('hex');
 
-  console.log(array)
+  // console.log(array)
 
-  if(code===signature) {
-    res.send(echostr);
-  } else {
-    res.send(echostr);
-  }
+  // if(code===signature) {
+  //   res.send(echostr);
+  // } else {
+  //   res.send(echostr);
+  // }
 });
 
 module.exports = router;
